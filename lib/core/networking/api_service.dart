964b9c334a193f63485ab58f6043b78ea474e0a4
yabdelfatah <yabdelfatah@ntgclarity.com>
2025-04-02 05:03:07 +0200

    ``` No code changes made; skipping commit. ```

--- lib/core/networking/api_service.dart
+++ lib/core/networking/api_service.dart
@@ -3,6 +3,9 @@ import 'package:retrofit/http.dart';
 import 'package:retrofit/retrofit.dart';
 import 'package:v_care/core/helpers/app_contants.dart';
 import 'package:v_care/core/networking/api_constants.dart';
+import 'package:v_care/features/auth/login/data/models/login_response_model.dart';
+
+import '../../features/auth/login/data/models/login_request_body.dart';
 part 'api_service.g.dart';
 
 // RestAPi=> Annotation for Retrofit , based on Dio library , used for API calls
@@ -13,7 +16,7 @@ abstract class ApiService {
   factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
 
   @POST(ApiConstants.login)
-  Future<String> login(
-    @Body() Map<String, dynamic> body,
+  Future<LoginResponseModel> login(
+    @Body() LoginRequestBody loginRequestBody,
   );
 }
