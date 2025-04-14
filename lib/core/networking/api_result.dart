f065a09a5aff9e3c0914fc11626c5efa9933dd6a
yusufafatah@gmail.com <yusufabdo20>
2025-04-14 08:20:30 +0200

    Implement login feature with repository pattern; update API result handling and state management

--- lib/core/networking/api_result.dart
+++ lib/core/networking/api_result.dart
@@ -1,10 +1,11 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:v_care/core/networking/api_error_handler.dart';
 
 part 'api_result.freezed.dart';
 
 @Freezed()
 abstract class ApiResult<T> with _$ApiResult<T> {
   const factory ApiResult.success(T data) = Success<T>;
-  const factory ApiResult.failure(String message) = Failure<T>;
+  const factory ApiResult.failure(ErrorHandler errorHandler) = Failure<T>;
 }
 //  String message Can Convert to Object of ErrorHandler , const factory ApiResult.failure(String message) = Failure<T>;
---
+++ lib/features/auth/login/logic/cubit/login_cubit.dart
@@ -0,0 +1,30 @@
+import 'package:bloc/bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:v_care/features/auth/login/data/models/login_request_body.dart';
+
+import '../../data/repo/login_repo.dart';
+import 'login_state.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  final LoginRepo _loginRepo;
+  LoginCubit(this._loginRepo) : super(const LoginState.initial());
+  void emitLoginStates(LoginRequestBody loginRequestBody) async {
+    emit(LoginState.loading());
+    final response = await _loginRepo.login(loginRequestBody);
+    //thhe type of the response is ApiResult<LoginResponseModel>
+    // ApiResult<LoginResponseModel> response = await _loginRepo.login(loginRequestBody);
+
+    response.when(
+      // response.when is a method from ###ApiResult### that takes two callbacks: one for success and one for failure.
+      // It is used to handle the response of the API call and emit the appropriate state based on the result.
+      success: (data) {
+        emit(LoginState.success(data));
+      },
+      failure: (error) {
+        emit(LoginState.failure(
+          message: error.apiErrorModel.message,
+        ));
+      },
+    );
+  }
+}
