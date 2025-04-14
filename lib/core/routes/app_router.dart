79c8360ae3e97f70293f7d040ac61e520a44dd2c
yusufafatah@gmail.com <yusufabdo20>
2025-04-14 09:51:12 +0200

    Add BlocProvider for LoginCubit in AppRouters; update login route handling

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:v_care/core/di/di.dart';
 import 'package:v_care/core/routes/routes.dart';
+import 'package:v_care/features/auth/login/logic/cubit/login_cubit.dart';
 
 import '../../features/auth/login/presentation/views/login/login_view.dart';
 import '../../features/on_boarding/views/on_boarding_view.dart';
@@ -10,7 +13,11 @@ class AppRouters {
       case Routes.onBoardingScreen:
         return MaterialPageRoute(builder: (context) => OnBoardingView());
       case Routes.loginScreen:
-        return MaterialPageRoute(builder: (context) => LoginView());
+        return MaterialPageRoute(
+            builder: (context) => BlocProvider(
+                  create: (context) => getIt<LoginCubit>(),
+                  child: LoginView(),
+                ));
       default:
         return MaterialPageRoute(
             builder: (context) => Scaffold(
